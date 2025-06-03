38e8da28368da43cf69a10087fa68aa6ffd66f7d
sumankaundinya <sumankaundinya@gmail.com>
2025-06-04 00:47:04 +0200

    React1-Week1 homework Galactica App

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -1,62 +1,43 @@
-import styles from './page.module.css';
+import styles from "./page.module.css";
+import OurValues from "./OurValues";
+import OurCrew from "./OurCrew";
+import OurPartners from "./OurPartners";
 
 // TASK - React 1 week 1
 // After you are finished with creating the page, move the OurValues, OurCrew, OurPartners components into their own files
 // OurValues.js, OurCrew.js, OurPartners.js should live in this folder
 // import and use the components from the newly created files
 
-const OurValues = () => {
-  // TASK - React 1 week 1
-  // Create the "Our Values" section
-  // Use the descriptions provided in /app/about_us/README.md
-  // Some inspiration ideas found in /data/inspiration_about_us
-  return (
-    <p> ADD OUR VALUES HERE </p>
-  );
-};
-
-const OurCrew = () => {
-  // TASK - React 1 week 1
-  // Create the "Our Crew section"
-  // Use the descriptions provided in /app/about_us/README.md
-  // Use the pictures from /public/crew
-  // Some inspiration ideas found in /data/inspiration_about_us
-  return (
-    <p> ADD OUR CREW HERE </p>
-  );
-}
-
-const OurPartners = () => {
-  // TASK - React 1 week 1
-  // Create the "Our Crew section"
-  // Use the descriptions provided in /app/about_us/README.md
-  // Use the pictures from /public/crew
-  // Some inspiration ideas found in /data/inspiration_about_us
-  return (
-    <p> ADD OUR Partners HERE </p>
-  );
-}
-
-
 export const Crew = () => {
   return (
     <div className="fullBGpicture">
       <main className="mainContent">
-        <h1>About us</h1>
-        <section className="card">
-          <h2>Our Values</h2>
-          <OurValues/>
+        <h1 className={styles.heading}>About us</h1>
+        <section className={styles.card}>
+          <h2 className={styles.sectionHeading}>Our Values</h2>
+          <br />
+          <OurValues />
         </section>
-        <section className="card">
-          <h2>The crew</h2>
-          <OurCrew/>
+        <section
+          className={styles.card}
+          style={{
+            backgroundImage: 'url("/crew/background-crew-desktop.jpg")',
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <h2 className={styles.sectionHeading}>The crew</h2>
+          <br />
+          <OurCrew />
         </section>
-
-         {/* TASK - React 1 week 1 */}
-         {/* Add in the "OurPartners" component here */}
+        <section className={styles.card}>
+          <h2 className={styles.sectionHeading}>Our Partners</h2>
+          <br />
+          <OurPartners />
+        </section>
       </main>
     </div>
   );
-}
+};
 
 export default Crew;
